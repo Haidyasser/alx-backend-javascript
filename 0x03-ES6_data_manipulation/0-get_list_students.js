a6a76d0bbdc1949d94a5a3018446c41b8f5d1518
Haidyasser <haidyasser3@gmail.com>
2024-10-09 11:44:07 +0300

    change task 0 implementation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,8 @@
+/* eslint-disable */
 export default function getListStudents() {
-  let students = [];
-  students.push({id: 1, firstName: 'Guillaume', location: 'San Francisco'});
-  students.push({id: 2, firstName: 'James', location: 'Columbia'});
-  students.push({id: 5, firstName: 'Serena', location: 'San Francisco'});
-  return students;
+  return [
+    { id: 1, firstName: "Guillaume", location: "San Francisco" },
+    { id: 2, firstName: "James", location: "Columbia" },
+    { id: 5, firstName: "Serena", location: "San Francisco" },
+  ];
 }
