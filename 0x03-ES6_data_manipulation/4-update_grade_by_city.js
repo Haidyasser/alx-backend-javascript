abccc5cefc58c654d5b45e8096a848e1fd1dce5c
Haidyasser <haidyasser3@gmail.com>
2024-10-09 12:38:10 +0300

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,13 @@
 export default function updateStudentGradeByCity(students, city, newGrade) {
   return students.filter((students) => students.location === city)
   .map((student) => {
+    let grade = "N/A";
+    if (newGrade.filter((gr) => gr.studentId === student.id).length > 0) {
+      grade = newGrade.filter((gr) => gr.studentId === student.id)[0].grade;
+    }
     return {
       ...student,
-      grade: newGrade.find((grade) => grade.studentId === student.id) ? newGrade.find((grade) => grade.studentId === student.id).grade : 'N/A',
+      grade,
     };
-  }
-  );
+  });
 }
