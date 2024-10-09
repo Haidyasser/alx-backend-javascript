16a9202b545cd7d5f0efebe53ad98188ac4119f5
Haidyasser <haidyasser3@gmail.com>
2024-10-09 13:02:51 +0300

    removing extra white space

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,13 @@
 export default function updateStudentGradeByCity(students, city, newGrade) {
   return students.filter((students) => students.location === city)
-  .map((student) => {
-    let grade = 'N/A';
-    if (newGrade.filter((gr) => gr.studentId === student.id).length > 0) {
-      grade = newGrade.filter((gr) => gr.studentId === student.id)[0].grade;
-    }
-    return {
-      ...student,
-      grade: grade,
-    };
+    .map((student) => {
+      let grade = 'N/A';
+      if (newGrade.filter((gr) => gr.studentId === student.id).length > 0) {
+        grade = newGrade.filter((gr) => gr.studentId === student.id)[0].grade;
+      }
+      return {
+        ...student,
+        grade: grade,
+      };
   });
 }
