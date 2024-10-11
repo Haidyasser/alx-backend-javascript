5270040c3a27f4d716102c690514c8fc70151907
Haidyasser <haidyasser3@gmail.com>
2024-10-11 12:31:27 +0300

    task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+import { array } from "yargs";
+import ClassRoom from "./0-classroom.js";
+
+export default function initializeRooms() {
+    return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
